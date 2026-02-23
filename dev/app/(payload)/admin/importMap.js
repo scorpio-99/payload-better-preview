fd9741e8f4d2e508cb8f0d9e05dafd52f23dfcfa
Scorpio-99 <luca-meli@outlook.com>
2026-02-23 15:44:01 +0100

    initial commit (from payload plugin)

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-better-preview/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-better-preview/rsc'
+
+export const importMap = {
+  'payload-better-preview/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-better-preview/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
